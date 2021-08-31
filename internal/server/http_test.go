bee027b690e6c5ca01cf1b4fb5ebadcb753da246
grmano <germano@poli.ufrj.br>
2021-08-31 09:47:16 -0300

    add test for http.go

--- internal/server/http_test.go
+++ internal/server/http_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 
-func TestHandleProduceBadJson(t *testing.T) {
+func TestHandleProduce200(t *testing.T) {
   s := newHTTPServer()
   req := httptest.NewRequest("POST", "http://test.com", strings.NewReader(`{"test":"oi"}`))
   w := httptest.NewRecorder()
@@ -17,5 +17,17 @@ func TestHandleProduceBadJson(t *testing.T) {
   resp := w.Result()
   if resp.StatusCode != 200 {
     t.Fatalf("Expected status code to be 200 got %v", resp.StatusCode)
+  }
+}
+
+func TestHandleProduceBadJson(t *testing.T) {
+  s := newHTTPServer()
+  req := httptest.NewRequest("POST", "http://test.com", strings.NewReader(`{"test":"oi"`))
+  w := httptest.NewRecorder()
+  s.handleProduce(w, req)
+
+  resp := w.Result()
+  if resp.StatusCode != 400 {
+    t.Fatalf("Expected status code to be 400 got %v", resp.StatusCode)
   }
 }
